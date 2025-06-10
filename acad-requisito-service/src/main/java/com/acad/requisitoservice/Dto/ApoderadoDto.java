425b44040112a920877729f183f3d6e773ee5005
NachyZuri <yukyuel@gmail.com>
2025-06-10 16:52:14 -0500

    update 1

---
+++ acad-requisito-service/src/main/java/com/acad/requisitoservice/Dto/ApoderadoDto.java
@@ -0,0 +1,11 @@
+package com.acad.requisitoservice.Dto;
+
+import lombok.Data;
+
+@Data
+public class ApoderadoDto {
+    private Long idApoderado;
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+}
